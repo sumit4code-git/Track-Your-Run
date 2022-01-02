c14ea6cb87ea487b25dc25589bec1b255dc63d7c
Sumit Jha <sumitjhaeluga@gmail.com>
2022-01-03 04:11:29 +0530

    Spinner Sort Type

--- app/src/main/java/com/example/runtracker/ui/fragment/RunFragment.kt
+++ app/src/main/java/com/example/runtracker/ui/fragment/RunFragment.kt
@@ -1,9 +1,9 @@
 package com.example.runtracker.ui.fragment
 
-import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -12,11 +12,11 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.runtracker.R
 import com.example.runtracker.adapter.RunAdapter
 import com.example.runtracker.other.Constants.REQUEST_CODE_LOCATION_PERMISSION
+import com.example.runtracker.other.SortType
 import com.example.runtracker.other.TrackingUtility
 import com.example.runtracker.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_run.*
-import kotlinx.android.synthetic.main.fragment_setup.*
 import pub.devrel.easypermissions.AppSettingsDialog
 import pub.devrel.easypermissions.EasyPermissions
 
@@ -30,8 +30,32 @@ class RunFragment:Fragment(R.layout.fragment_run),EasyPermissions.PermissionCall
         super.onViewCreated(view, savedInstanceState)
         requestPermissions()
         setUpRecyclerView()
+        initComponents()
+    }
 
-        viewModel.runsSortedByDate.observe(viewLifecycleOwner, Observer {
+    private fun initComponents() {
+//        To sort According to  initial selection of spinner
+        when(viewModel.sortType){
+            SortType.DATE->spFilter.setSelection(1)
+            SortType.RUNNING_TIME->spFilter.setSelection(1)
+            SortType.DISTANCE->spFilter.setSelection(2)
+            SortType.CALORIES_BURNT->spFilter.setSelection(4)
+            SortType.AVG_SPEED->spFilter.setSelection(3)
+        }
+        spFilter.onItemSelectedListener=object :AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, pos: Int, id: Long) {
+                when(pos){
+                    0-> viewModel.sortRuns(SortType.DATE)
+                    1-> viewModel.sortRuns(SortType.RUNNING_TIME)
+                    2-> viewModel.sortRuns(SortType.DISTANCE)
+                    3-> viewModel.sortRuns(SortType.AVG_SPEED)
+                    4-> viewModel.sortRuns(SortType.CALORIES_BURNT)
+                }
+            }
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+
+        }
+        viewModel.runs.observe(viewLifecycleOwner, Observer {
             runAdapter.submitList(it)
         })
 
@@ -39,7 +63,8 @@ class RunFragment:Fragment(R.layout.fragment_run),EasyPermissions.PermissionCall
             findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
         }
     }
-//Setting RecyclerView To show saved runs in database
+
+    //Setting RecyclerView To show saved runs in database
     private fun setUpRecyclerView()=rvRuns.apply {
         runAdapter=RunAdapter()
         adapter=runAdapter
--- app/src/main/java/com/example/runtracker/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/runtracker/ui/viewmodels/MainViewModel.kt
@@ -1,8 +1,10 @@
 package com.example.runtracker.ui.viewmodels
 
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.runtracker.db.Run
+import com.example.runtracker.other.SortType
 import com.example.runtracker.repositories.MainRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -13,7 +15,55 @@ class MainViewModel @Inject constructor(
     val mainRepository: MainRepository
 ):ViewModel(){
 
-    val runsSortedByDate=mainRepository.getAllRunsSortedByDate()
+    private val runsSortedByDate=mainRepository.getAllRunsSortedByDate()
+    private val runsSortedByDistance=mainRepository.getAllRunsSortedByDistance()
+    private val runsSortedByCalorieBurnt=mainRepository.getAllRunsSortedByCaloriesBurned()
+    private val runsSortedByTimeInMillis=mainRepository.getAllRunsSortedByTimeInMillis()
+    private val runsSortedByAvgSpeed=mainRepository.getAllRunsSortedByAvgSpeed()
+
+//    Mediator Live Data because we can observe to only one  live data  and need to change the value according to spinner choices depending upon user selection
+    val runs=MediatorLiveData<List<Run>>()
+
+    var sortType=SortType.DATE
+
+    init {
+        runs.addSource(runsSortedByDate){result->
+            if(sortType==SortType.DATE){
+                result?.let { runs.value=it }
+            }
+        }
+        runs.addSource(runsSortedByDistance){result->
+            if(sortType==SortType.DISTANCE){
+                result?.let { runs.value=it }
+            }
+        }
+        runs.addSource(runsSortedByCalorieBurnt){result->
+            if(sortType==SortType.CALORIES_BURNT){
+                result?.let { runs.value=it }
+            }
+
+        }
+        runs.addSource(runsSortedByAvgSpeed){result->
+            if(sortType==SortType.AVG_SPEED){
+                result?.let { runs.value=it }
+            }
+        }
+        runs.addSource(runsSortedByTimeInMillis){result->
+            if(sortType==SortType.RUNNING_TIME){
+                result?.let { runs.value=it }
+            }
+        }
+    }
+//    to change live data of observer according to change after initialization
+    fun sortRuns(sortType : SortType) = when(sortType){
+        SortType.DATE->runsSortedByDate.value?.let{ runs.value= it}
+        SortType.RUNNING_TIME->runsSortedByTimeInMillis.value?.let{ runs.value= it}
+        SortType.AVG_SPEED->runsSortedByAvgSpeed.value?.let{ runs.value= it}
+        SortType.CALORIES_BURNT->runsSortedByCalorieBurnt.value?.let{ runs.value= it}
+        SortType.DISTANCE->runsSortedByDistance.value?.let{ runs.value= it}
+    }.also {
+        this.sortType = sortType
+    }
    fun insertRun(run: Run)=viewModelScope.launch {
        mainRepository.insertRun(run)
    }
