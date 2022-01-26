1aa7449054fbd502b03e9e2eb43df6660dc4bb2e
Sumit Jha <sumitjhaeluga@gmail.com>
2022-01-27 00:02:41 +0530

    Code Cleaned

--- app/src/main/java/com/example/runtracker/SplashScreen.kt
+++ app/src/main/java/com/example/runtracker/SplashScreen.kt
@@ -7,12 +7,14 @@ import android.os.Handler
 import android.view.WindowManager
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.runtracker.ui.MainActivity
 import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreen : AppCompatActivity() {
     private lateinit var top:Animation
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
         // This is used to hide the status bar and make
@@ -21,17 +23,15 @@ class SplashScreen : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
-        top = AnimationUtils.loadAnimation(this, R.anim.top_animation)
-//        bottom = AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
-//        detail.setAnimation(bottom)
-        heading.setAnimation(top)
-
         // we used the postDelayed(Runnable, time) method
         // to send a message with a delayed time.
         Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 2500) // 3000 is the delayed time in milliseconds.
+        }, 2800) // 3000 is the delayed time in milliseconds.
+//        Set Up Text Animation
+        top = AnimationUtils.loadAnimation(this, R.anim.top_animation)
+        heading.setAnimation(top)
     }
 }
